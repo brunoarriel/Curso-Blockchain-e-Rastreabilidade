22698eb17b6f70d3f4c432b47d804b7227292c67
brunoarriel <brunoarriel@ufla.br>
2025-11-08 23:54:41 -0300

    "Criada a função registra Animal"

--- contracts/RegistraAnimais.sol
+++ contracts/RegistraAnimais.sol
@@ -16,6 +16,23 @@ contract RegistraAnimais {
     // Mapeamentos para facilitar buscas no contrato
     // Registra os animais pelo seu número de identificação
     mapping(uint256 => Animal) public animais;  
+
+    // Registro de movimentação do animal
+    uint256[][] public movAnimal;
+
+    // Texa para abertura de um leilão
+    uint256  txLeilao = 100;
+
+    function registraAnimal(string memory _nome, uint256 _codPropriedade) public {
+
+        ultimoRegistro++; // Gerando o número de identificação do animal
+        animais[ultimoRegistro] = Animal(
+            ultimoRegistro, // Salvando o número de identificação gerado
+            msg.sender, // Recuperando o endereço da carteira do proprietário            
+            _nome, // Salvando o Nome do Animal Registrado
+            _codPropriedade); // Salvando o codPropriedade
+    } 
+
     
 
 }
